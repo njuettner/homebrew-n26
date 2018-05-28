8c74acb5516ca3fdfe2b37c6b1373d9cd3e26b36
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-28 14:35:23 +0200

    Brew formula update for n26 version v0.4

--- Formula/n26.rb
+++ Formula/n26.rb
@@ -1,9 +1,9 @@
 class N26 < Formula
   desc "N26 CLI"
   homepage "https://github.com/njuettner/n26"
-  url "https://github.com/njuettner/n26/releases/download/v0.3/n26_0.3_darwin_amd64.tar.gz"
-  version "0.3"
-  sha256 "b6fdd673927ef97f1ff8f1f310ecf7b7f6403d30b752a3414379541eb623c5ed"
+  url "https://github.com/njuettner/n26/releases/download/v0.4/n26_0.4_darwin_amd64.tar.gz"
+  version "0.4"
+  sha256 "b1ddf0b922d9df4ae7bf60bb8823b612db0dae38f5f260701ffc4e5704c03603"
 
   def install
     bin.install "n26"
